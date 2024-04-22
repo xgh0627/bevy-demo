8c640746f1aa30f1a1d407d1f1a6f86d3b29a6f0
xgh0627 <417644978@qq.com>
2024-04-22 22:17:24 +0800

    add img

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main(){
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup,spawn_scene)
+        // .add_systems(Update,move_cube)
         .run();
 }
 
@@ -11,30 +12,37 @@ fn main(){
 struct Cube;
 
 fn spawn_scene(mut commands: Commands,mut meshes: ResMut<Assets<Mesh>>,mut materials: ResMut<Assets<StandardMaterial>>) {
+    // circular base
     commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube::new(1.0))),
-        material: materials.add(bevy::prelude::Color::BEIGE.into()),
-        transform: Transform::from_xyz(0.0,0.5,0.0),
+        mesh: meshes.add(Circle::new(6.0)),
+        material: materials.add(Color::WHITE),
+        transform: Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
         ..default()
-    })
-        .insert(Cube);
-
-    let player_camera_y_offest: f32 = 20.0;
-    let player_camera_z_offest: f32 = 10.0;
-
-    commands.spawn(Camera3dBundle{
-        transform: Transform::from_xyz(0.0,player_camera_y_offest,player_camera_z_offest).looking_at(Vec3::ZERO,Vec3::Y),
+    }).insert(Cube);
+    // cube
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
+        material: materials.add(Color::rgb_u8(124, 144, 255)),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
         ..default()
     });
-
-    commands.spawn(PbrBundle{
-       mesh: meshes.add(Mesh::from(shape::Plane::from_size(15.0))),
-        material: materials.add(bevy::prelude::Color::SEA_GREEN.into()),
+    // light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
         ..default()
     });
-
-    commands.insert_resource(AmbientLight {
-        color: Default::default(),
-        brightness: 1.0
+    // camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
     });
 }
+
+// fn move_cube(mut query: Query<&mut Transform,With<Cube>>) {
+//     let mut transform = query.single_mut();
+//     transform.translation.x += 0.05;
+// }
